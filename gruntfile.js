e335b4e918a37324eef6eb135a42a786f9d79079
Dean <deanmono@gmail.com>
2016-02-09 00:51:18 -0600

    concat remove

--- gruntfile.js
+++ gruntfile.js
@@ -55,7 +55,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
 };
